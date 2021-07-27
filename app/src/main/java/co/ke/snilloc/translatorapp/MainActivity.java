f5a1eb949ef55edcd0ac09c195a8cd5164d43867
loop <loopinfinity@criptext.com>
2021-07-27 12:09:58 +0300

    add splash screen feature with animation

--- app/src/main/java/co/ke/snilloc/translatorapp/MainActivity.java
+++ app/src/main/java/co/ke/snilloc/translatorapp/MainActivity.java
@@ -2,13 +2,45 @@ package co.ke.snilloc.translatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.accounts.Account;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    //hook views
+    @BindView(R.id.MainAppNameTextView) TextView mMainAppNameTextView;
+    @BindView(R.id.MainAppLoadingTextView) TextView mMainAppLoadingTextView;
+
+    Animation topAnim,blinkAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        blinkAnim = AnimationUtils.loadAnimation(this,R.anim.blink_animation);
+
+        mMainAppNameTextView.setAnimation(topAnim);
+        mMainAppLoadingTextView.setAnimation(blinkAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, AccountActivity.class);
+                startActivity(intent);
+                //kill this
+                finish();
+            }
+        },5000);
     }
 }
