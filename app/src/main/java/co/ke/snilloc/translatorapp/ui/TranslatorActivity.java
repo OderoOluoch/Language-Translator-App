5bed665cc7a605e96b1612e96767e007d9a8f5f7
loop <loopinfinity@criptext.com>
2021-07-28 15:15:30 +0300

    add translate feature

--- app/src/main/java/co/ke/snilloc/translatorapp/ui/TranslatorActivity.java
+++ app/src/main/java/co/ke/snilloc/translatorapp/ui/TranslatorActivity.java
@@ -2,6 +2,10 @@ package co.ke.snilloc.translatorapp.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -10,12 +14,16 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.mannan.translateapi.Language;
+import com.mannan.translateapi.TranslateAPI;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import co.ke.snilloc.translatorapp.R;
 
-public class TranslatorActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener,View.OnClickListener {
+public class TranslatorActivity extends AppCompatActivity {
     @BindView(R.id.TextInputEditText) EditText mTextInputEditText;
     @BindView(R.id.spinner) Spinner mSpinner;
     @BindView(R.id.TranslateButton) Button mTranslateButton;
@@ -36,30 +44,34 @@ public class TranslatorActivity extends AppCompatActivity implements AdapterView
 
         // Apply the adapter to the spinner
         mSpinner.setAdapter(adapter);
+
+        mTranslateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userInput = mTextInputEditText.getText().toString();
+                String userLanguage = mSpinner.getSelectedItem().toString();
+
+
+
+                if (userInput.isEmpty()){
+                    Toast.makeText(TranslatorActivity.this, "Enter text to translate", Toast.LENGTH_SHORT).show();
+                }else{
+                    TranslateAPI translateAPI = new TranslateAPI(Language.AUTO_DETECT,Language.AFRIKAANS,userInput);
+
+                    translateAPI.setTranslateListener(new TranslateAPI.TranslateListener() {
+                        @Override
+                        public void onSuccess(String translatedText) {
+                            mTextTranslatedTextView.setText(translatedText);
+                        }
+
+                        @Override
+                        public void onFailure(String ErrorText) {
+                            Toast.makeText(TranslatorActivity.this, "Failed try again later!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        });
     }
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
-        // An item was selected. You can retrieve the selected item using
-        // parent.getItemAtPosition(pos)
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-        // Another interface callback
-
-    }
-
-    @Override
-    public void onClick(View v) {
-
-        if (v == mTranslateButton){
-            translateText();
-        }
-    }
-
-    private void translateText() {
-    }
 }
